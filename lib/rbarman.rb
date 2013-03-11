20b27d02b4db1f7e5f201d6db1a024ccf276fe05
Holger Amann <holger@sauspiel.de>
2013-03-11 23:45:03 +0100

    documenting module

--- lib/rbarman.rb
+++ lib/rbarman.rb
@@ -6,5 +6,6 @@ require "rbarman/wal_files"
 require "rbarman/cli_command"
 require "rbarman/configuration"
 
+# The RBarman module contains all aspects of backups made by barman and dealing with them
 module RBarman
 end
